dc6f12814945f8e4cf5a83d7ec768d6a428ea82d
chnhuangyan <chnhuangyan@gmail.com>
2021-04-16 20:15:36 -0400

    Add files via upload

---
+++ aspnet-core/src/AngularDotNetCore.HttpApi.Host/AngularDotNetCoreBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace AngularDotNetCore
+{
+    [Dependency(ReplaceServices = true)]
+    public class AngularDotNetCoreBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "AngularDotNetCore";
+    }
+}
